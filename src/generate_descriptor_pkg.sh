3a1c3354050d6e7eae52bbb0cd6e52860c7c4191
Austin Cormier <Austin.Cormier@riftio.com>
2016-05-23 21:46:17 -0400

    Add apache license to newly added files.

--- src/generate_descriptor_pkg.sh
+++ src/generate_descriptor_pkg.sh
@@ -1,7 +1,22 @@
 #! /usr/bin/bash
-# STANDARD_RIFT_IO_COPYRIGHT
-# Author(s): Anil Gunturu
-# Creation Date: 2015/10/09
+#
+#   Copyright 2016 RIFT.IO Inc
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Author(s): Austin Cormier
+# Creation Date: 2016/05/23
+#
 #
 # This shell script is used to create a descriptor package
 # The main functions of this script include:
