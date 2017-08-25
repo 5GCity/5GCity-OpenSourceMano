0bff497a70fbf700d31e79ed2e212bb4d1b4639b
Mike Marchetti <mmarchetti@sandvine.com>
2017-08-25 10:06:49 -0400

    define stage for stage_1
    Change-Id: I5a7a11b6ce2d542e583da86cc917e0e5852b4d74
    Signed-off-by: Mike Marchetti <mmarchetti@sandvine.com>

--- jenkins/ci-pipelines/ci_stage_1.groovy
+++ jenkins/ci-pipelines/ci_stage_1.groovy
@@ -34,25 +34,27 @@ node {
         params.PROJECT_URL_PREFIX  = 'https://osm.etsi.org/gerrit'
     }
 
-    // pipeline running from gerrit trigger.
-    // kickoff the downstream multibranch pipeline
-    def downstream_params = [
-        string(name: 'GERRIT_BRANCH', value: GERRIT_BRANCH),
-        string(name: 'GERRIT_PROJECT', value: GERRIT_PROJECT),
-        string(name: 'GERRIT_REFSPEC', value: GERRIT_REFSPEC),
-        string(name: 'GERRIT_PATCHSET_REVISION', value: GERRIT_PATCHSET_REVISION),
-        string(name: 'PROJECT_URL_PREFIX', value: params.PROJECT_URL_PREFIX),
-        booleanParam(name: 'TEST_INSTALL', value: params.TEST_INSTALL),
-    ]
+    stage('downstream stage_2') {
+        // pipeline running from gerrit trigger.
+        // kickoff the downstream multibranch pipeline
+        def downstream_params = [
+            string(name: 'GERRIT_BRANCH', value: GERRIT_BRANCH),
+            string(name: 'GERRIT_PROJECT', value: GERRIT_PROJECT),
+            string(name: 'GERRIT_REFSPEC', value: GERRIT_REFSPEC),
+            string(name: 'GERRIT_PATCHSET_REVISION', value: GERRIT_PATCHSET_REVISION),
+            string(name: 'PROJECT_URL_PREFIX', value: params.PROJECT_URL_PREFIX),
+            booleanParam(name: 'TEST_INSTALL', value: params.TEST_INSTALL),
+        ]
 
-    println("TEST_INSTALL = ${params.TEST_INSTALL}")
-    // callout to stage_2.  This is a multi-branch pipeline.
-    upstream_job_name = "${mdg}-stage_2/${GERRIT_BRANCH}"
+        println("TEST_INSTALL = ${params.TEST_INSTALL}")
+        // callout to stage_2.  This is a multi-branch pipeline.
+        upstream_job_name = "${mdg}-stage_2/${GERRIT_BRANCH}"
 
-    stage_2_result = build job: "${upstream_job_name}", parameters: downstream_params, propagate: true
-    if (stage_2_result.getResult() != 'SUCCESS') {
-        project = stage_2_result.getProjectName()
-        build = stage_2_result.getNumber()
-        error("${project} build ${build} failed")
+        stage_2_result = build job: "${upstream_job_name}", parameters: downstream_params, propagate: true
+        if (stage_2_result.getResult() != 'SUCCESS') {
+            project = stage_2_result.getProjectName()
+            build = stage_2_result.getNumber()
+            error("${project} build ${build} failed")
+        }
     }
 }
