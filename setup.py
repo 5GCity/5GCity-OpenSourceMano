825b76da2d51e30cc0fc79443dd60be4b0555c13
garciadeblas <gerardo.garciadeblas@telefonica.com>
2017-08-30 15:03:32 +0200

    Added description to setup.py
    Change-Id: Id5440b60b6dcea6e36e5275742dab703a900e049
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='osm_im',
+    description='OSM Information Model',
+    long_description = open('README.rst').read(),
     version_command=('git describe --tags --long --dirty', 'pep440-git'),
     author='Mike Marchetti',
     author_email='mmarchetti@sandvine.com',
