d92834a7aa90e10ef67867d1e095ff0e01e871b4
Mike Marchetti <mmarchetti@sandvine.com>
2017-09-09 14:42:55 -0400

    add jenkin user to dockerfile on build
    Change-Id: I47fc730d27ff852d94b024be4e11adf3817aa011
    Signed-off-by: Mike Marchetti <mmarchetti@sandvine.com>

--- jenkins/ci-pipelines/ci_stage_2.groovy
+++ jenkins/ci-pipelines/ci_stage_2.groovy
@@ -45,16 +45,14 @@ def ci_pipeline(mdg,url_prefix,project,branch,refspec,revision,build_system) {
     container_name = "${project}-${branch}".toLowerCase()
 
     stage('Docker-Build') {
+        sh '''
+           echo RUN groupadd -o -g $(id -g) -r jenkins >> Dockerfile
+           echo RUN useradd -o -u $(id -u) --create-home -r -g  jenkins jenkins >> Dockerfile
+           '''
         sh "docker build -t ${container_name} ."
     }
 
     withDockerContainer("${container_name}") {
-        stage('Docker-Setup') {
-            sh '''
-               groupadd -o -g $(id -g) -r jenkins
-               useradd -o -u $(id -u) --create-home -r -g  jenkins jenkins
-               '''
-        }
         stage('Test') {
             sh 'devops-stages/stage-test.sh'
         }
