3b3edeabc440a596c05ba6e10cdca2bf074c3a86
Jeremy Mordkoff <Jeremy.Mordkoff@riftio.com>
2017-10-03 16:21:56 -0400

    set the RIFT_EXTERNAL_ADDRESS to the address of the parent container/VM
    Signed-off-by: Jeremy Mordkoff <Jeremy.Mordkoff@riftio.com>

--- installers/install_osm.sh
+++ installers/install_osm.sh
@@ -188,6 +188,8 @@ function configure(){
     echo -e "       Configuring SO"
     sudo route add -host $JUJU_CONTROLLER_IP gw $VCA_CONTAINER_IP
     sudo sed -i "$ i route add -host $JUJU_CONTROLLER_IP gw $VCA_CONTAINER_IP" /etc/rc.local
+    echo RIFT_EXTERNAL_ADDRESS=$DEFAULT_IP | lxc exec SO-ub -- tee -a /usr/rift/etc/default/launchpad
+
     lxc exec SO-ub -- systemctl restart launchpad
 
     so_is_up $SO_CONTAINER_IP
