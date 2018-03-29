034a5305e1535971979b842e4ee73bee40d8b1af
diazb <bdiaz@whitestack.com>
2018-03-29 19:17:09 -0300

    Adds license header to python files
    Signed-off-by: diazb <bdiaz@whitestack.com>

--- policy_module/osm_policy_module/common/alarm_config.py
+++ policy_module/osm_policy_module/common/alarm_config.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Whitestack, LLC
+# *************************************************************
+
+# This file is part of OSM Monitoring module
+# All Rights Reserved to Whitestack, LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+
+#         http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# For those usages not covered by the Apache License, Version 2.0 please
+# contact: bdiaz@whitestack.com or glavado@whitestack.com
+##
+
+
 class AlarmConfig:
     def __init__(self, metric_name, resource_uuid, vim_uuid, threshold, operation, statistic, action):
         self.metric_name = metric_name
