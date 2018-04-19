feef887387cd7bde1b98617cb9ecdf5a9e4d4537
garciadeblas <gerardo.garciadeblas@telefonica.com>
2018-04-19 13:35:25 +0200

    install_osm.sh: echo proper env variables when installing osmclient
    Change-Id: I8e559650502f9b083b0b06b6bd45159f3ecf8831
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- installers/install_osm.sh
+++ installers/install_osm.sh
@@ -424,6 +424,9 @@ function install_osmclient(){
     #sed 's,OSM_SOL005=[^$]*,OSM_SOL005=True,' -i ${HOME}/.bashrc
     #echo 'export OSM_HOSTNAME=localhost' >> ${HOME}/.bashrc
     #echo 'export OSM_SOL005=True' >> ${HOME}/.bashrc
+    [ -z "$INSTALL_LIGHTWEIGHT" ] && export OSM_HOSTNAME=`lxc list | awk '($2=="SO-ub"){print $6}'`
+    [ -z "$INSTALL_LIGHTWEIGHT" ] && export OSM_RO_HOSTNAME=`lxc list | awk '($2=="RO"){print $6}'`
+    [ -n "$INSTALL_LIGHTWEIGHT" ] && export OSM_HOSTNAME=localhost
     echo -e "\nOSM client installed"
     echo -e "You might be interested in adding the following OSM client env variables to your .bashrc file:"
     echo "     export OSM_HOSTNAME=${OSM_HOSTNAME}"
