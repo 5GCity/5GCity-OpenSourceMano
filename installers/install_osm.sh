35f96ae47e56c03c837c31fabbd516d350eb295e
garciadeblas <gerardo.garciadeblas@telefonica.com>
2018-05-23 12:33:36 +0200

    install_osm: apt-get update before installing devops
    Change-Id: I786bb2b67a049fd93916e7fd449940d9ac071ac5
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- installers/install_osm.sh
+++ installers/install_osm.sh
@@ -17,5 +17,6 @@ add_repo() {
 }
 
 add_repo "deb [arch=amd64] $REPOSITORY_BASE/$RELEASE $REPOSITORY devops"
-sudo DEBIAN_FRONTEND=noninteractive  apt-get install osm-devops
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install osm-devops
 /usr/share/osm-devops/installers/full_install_osm.sh -R $RELEASE -r $REPOSITORY -D /usr/share/osm-devops "$@"
