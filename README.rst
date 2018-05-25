6b70e44238fa285d7b4d16aeac2277d0768ee693
garciadeblas <gerardo.garciadeblas@telefonica.com>
2018-05-25 15:50:58 +0200

    setup.py: version_command instead of static version; added MANIFEST.in
    Change-Id: I115c3eef0e63cdaacfb88199d93bcd77071e6657
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- README.rst
+++ README.rst
@@ -2,5 +2,5 @@
 osm-lcm
 ===========
 
-osm-lcm is the Lightweight Build Life Cycle Management for OSM. It interact with RO module for resource orchestration and N2VC for resoruce orchestration
+osm-lcm is the Lightweight Build Life Cycle Management for OSM. It interact with RO module for resource orchestration and N2VC for VNF configuration
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 _name = "osm_lcm"
 # version is at first line of osm_lcm/html_public/version
 here = os.path.abspath(os.path.dirname(__file__))
-VERSION = "4.0.1rc1"
+# VERSION = "4.0.1rc1"
 with open(os.path.join(here, 'README.rst')) as readme_file:
     README = readme_file.read()
 
@@ -14,8 +14,8 @@ setup(
     name=_name,
     description='OSM North Bound Interface',
     long_description=README,
-    # version_command=('git describe --tags --long --dirty', 'pep440-git'),
-    version=VERSION,
+    version_command=('git describe --tags --long --dirty', 'pep440-git'),
+    # version=VERSION,
     # python_requires='>3.5.0',
     author='ETSI OSM',
     author_email='alfonso.tiernosepulveda@telefonica.com',
@@ -27,7 +27,6 @@ setup(
     packages=[_name],
     include_package_data=True,
     data_files=[('/etc/osm/', ['osm_lcm/lcm.cfg']),
-                ('/etc/systemd/system/', ['osm_lcm/osm-lcm.service']),
                 ],
     dependency_links=[
         "git+https://osm.etsi.org/gerrit/osm/common.git@master#egg=osm-common-0.1.4"
@@ -36,7 +35,7 @@ setup(
         'pymongo', 'PyYAML',
         # 'osm-common',
     ],
-    # setup_requires=['setuptools-version-command'],
+    setup_requires=['setuptools-version-command'],
     # test_suite='nose.collector',
     # entry_points='''
     #     [console_scripts]
